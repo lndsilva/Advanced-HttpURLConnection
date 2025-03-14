738d1081df71378519cdfb76c9357ce2aee19326
lndsilva <lndsilva@hotmail.com>
2025-03-14 16:40:49 -0300

    menu com cardview e cardcicle

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -37,6 +37,8 @@ dependencies {
     implementation(libs.material)
     implementation(libs.activity)
     implementation(libs.constraintlayout)
+    implementation (libs.volley)
+    implementation (libs.circleimageview)
     testImplementation(libs.junit)
     androidTestImplementation(libs.ext.junit)
     androidTestImplementation(libs.espresso.core)
